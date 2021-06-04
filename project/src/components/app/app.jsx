0b32350233b7c55d690096701d22ba5023dbef3a
nstsautchenkova <nst.sautchenkova@gmail.com>
2021-06-04 16:17:50 +0300

    module2-task1 - Маршрут по тайным тропам

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,14 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { AppRoute } from '../../const.js';
 import HomePage from '../homePage/homePage.jsx';
-
+import LoginPage from '../loginPage/loginPage.jsx';
+import FavoritesPage from '../favoritesPage/favoritesPage.jsx';
+import RoomPage from '../roomPage/roomPage.jsx';
+import Page404 from '../404/404.jsx';
 function App(props) {
   const { rentalOfferCout, cardsCout } = props;
   return (
-    <HomePage
-      rentalOfferCout={rentalOfferCout}
-      cardsCout={cardsCout}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.MAIN}>
+          <HomePage
+            rentalOfferCout={rentalOfferCout}
+            cardsCout={cardsCout}
+          />
+        </Route>
+        <Route exact path={AppRoute.SIGN_IN}>
+          <LoginPage />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <FavoritesPage />
+        </Route>
+        <Route exact path="/offer/:id?" component={RoomPage} />
+        <Route>
+          <Page404 />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ project/src/components/header/header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../logo/logo.jsx';
+
+function Header(props) {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div className="header__left">
+            <Logo />
+          </div>
+          <nav className="header__nav">
+            <ul className="header__nav-list">
+              <li className="header__nav-item user">
+                <Link className="header__nav-link header__nav-link--profile" to='/login'>
+                  <div className="header__avatar-wrapper user__avatar-wrapper">
+                  </div>
+                  <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                </Link>
+              </li>
+              <li className="header__nav-item">
+                <Link className="header__nav-link" to='/login'>
+                  <span className="header__signout">Sign out</span>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
